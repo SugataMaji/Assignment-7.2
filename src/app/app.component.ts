698d13d6a8bf1bfd198e0552d5dac039fa170eb9
SugataMaji <sugatamaji94@gmail.com>
2017-08-15 01:23:42 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title="Click me";
+  name="sugata";
+  lastname="maji";
+
+  product: Array<Object>=[
+    {
+      title:"Phone",
+      price: 2000
+    },
+    {
+      title:"Bike",
+      price: 50000
+    },
+    {
+      title:"Car",
+      price: 200000
+    },
+  ];
+  
+
+ onbtnclick(){
+     this.title="changed"
+      this.title="changed"
+
+    }
+
+  }
